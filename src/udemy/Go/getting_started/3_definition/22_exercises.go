d046f81f715f5d79e01e941ab300a1edf3563b80
heku777 <chotips08@gmail.com>
2021-06-07 10:32:17 +0900

    #1 add files and #16 update files

---
+++ src/udemy/Go/getting_started/3_definition/22_exercises.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	// Q1
+	fmt.Println("##########Q1##########")
+	f := 1.11
+	ff := int(f)
+	fmt.Println(f)
+	fmt.Println(ff)
+
+	//Q2
+	fmt.Println("##########Q2##########")
+
+
+}
